019bf950960c3ed5b7519c6e3b6ae5ca619bb71f
Justin Pacubas <jpacubas@gmail.com>
2017-11-24 13:06:46 -0600

    chore: add urls to be analyzed

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const rpn = require('request-promise-native')
 const async = require('async')
+
+const urls = [
+  'http://www.24x7mag.com/2017/11/focus-year-ahead/?ref=fr-title',
+  'http://www.24x7mag.com/2017/10/cleveland-clinic-unveils-top-medical-innovations-2018/?ref=fr-title',
+  'http://www.24x7mag.com/2017/11/global-medical-imaging-device-market-slated-reach-74-billion'
+]
